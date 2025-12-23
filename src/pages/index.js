3ca9e3453d7821fc938d17d62735da99c98aa4cd
Mohamed Mahmoud <m.mahmoud2363@nu.edu.eg>
2025-12-23 22:15:30 +0200

    Updating frame for about us page / remove footer

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HowItWorks from './HowItWorks';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HowItWorks />
+  </React.StrictMode>
+);
+
